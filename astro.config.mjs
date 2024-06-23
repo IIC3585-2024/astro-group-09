1ba42e54c8450279ebccc38c1546b66ff0fd5cad
sbvillalobosm <svillalobosm@uc.cl>
2024-06-22 23:34:48 -0400

    new version

--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-export default defineConfig({
-  integrations: [react()],
-  buildOptions: {
-    site: 'http://localhost:3000',
-  },
-});
